2edda3e0c89f4316e338e1d7c62a2bd5b3dbe1c6
Zhenjian Zhang <itdesk.zhang@gmail.com>
2021-01-10 16:17:52 +0800

    初始版本
    Signed-off-by: Zhenjian Zhang <itdesk.zhang@gmail.com>

---
+++ AUCloudflareIP.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+#set -x
+version="1.0"
+Script_file="/usr/share/AUCloudflareIP"
+red="\033[31m"
+green="\033[32m"
+yellow="\033[33m"
+white="\033[0m"
+
+	
+start() {
+	cd  $Script_file
+	echo "----------------------------------------------"
+	echo -e "$green AUCloudflareIP $version $white"
+	echo "----------------------------------------------"
+	echo -e "$green 当前时间：$white`date "+%Y-%m-%d %H:%M"`"
+	if [ -f old_ip.txt ]; then
+		echo ""		
+	else
+		clear
+		read  -p "$green请输入你现在酸酸使用的IP或者域名：$white" suansuan
+		echo $suansuan > old_ip.txt
+	fi
+	
+	./CloudflareST -p 1 -o result.txt
+	
+	if [ ! result.txt ]; then
+		echo "文件为空，不做改变"
+	else
+		suansuan=$(cat old_ip.txt)
+		new_ip=$(cat result.txt | awk -F '[ ,]+' 'NR==2 {print $1}')
+		sed -i "s/$suansuan/$new_ip/g" /etc/config/shadowsocksr
+		/etc/init.d/shadowsocksr restart
+		echo -e "$green将旧IP:$white${suansuan}$green替换为新IP:$white${new_ip}$green,并重启酸酸$white"
+		echo $new_ip > old_ip.txt
+
+		cron_if=$(cat /etc/crontabs/root | grep "AUCloudflareIP" |wc -l)
+		if [ $cron_if  = "1" ]; then
+			echo ""		
+		else
+			echo "30 10 * * * /usr/share/AUCloudflareIP.sh >/tmp/AUCloudflareIP.log 2>&1" >>/etc/crontabs/root
+			echo "45 10 * * * /usr/share/AUCloudflareIP.sh update_script >/tmp/AUCloudflareIP.log 2>&1" >>/etc/crontabs/root
+		fi
+	fi
+}
+
+update_script() {
+	cd $Script_file
+	git_pull
+	chmod +x AUCloudflareIP.sh
+}
+
+
+action1="$1"
+if [ -z $action1 ]; then
+	start
+else
+	case "$action1" in
+			start|update_script)
+			$action1
+			;;
+			*)
+			echo "请不要乱输，我是不会执行的。。。"
+			exit
+			;;
+esac
+fi
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AUCloudflareIP
-调用XIU2/CloudflareSpeedTest，并自动替换openwrt上的酸酸地址
+调用https://github.com/XIU2/CloudflareSpeedTest得到新的IP，并自动替换openwrt上的酸酸地址
+
+
+## 支持系统
+openwrt X86 （基于x86测试）
+
+## Usage 使用方法
+```sh
+git clone https://github.com/ITdesk01/AUCloudflareIP.git /usr/share/AUCloudflareIP
+cd /usr/share/AUCloudflareIP && chmod +x AUCloudflareIP.sh
+sh AUCloudflareIP.sh
+```
+
